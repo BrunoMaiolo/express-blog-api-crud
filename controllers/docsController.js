bd26b982ff9afe16f896eb074c51caa972efedb6
BrunoMaiolo <brunosj@hotmail.it>
2026-03-03 16:41:14 +0100

    Milenstone 2

--- controllers/docsController.js
+++ controllers/docsController.js
@@ -1,10 +1,18 @@
+const posts = require("../data/posts.js");
+
+
+
 const index = (req, res) => {
-    res.send("Lista dei post");
+    res.json(posts);
 };
 
 const show = (req, res) => {
-    const id = req.params.id;
-    res.send(`Dettaglio del post ${id}`);
+    const id = parseInt(req.params.id);
+    const post = posts.find(p => p.id === id);
+    if (!post) {
+        return res.status(404).json({ error: "Post not found" });
+    }
+    res.json(post);
 };
 
 const store = (req, res) => {
@@ -17,8 +25,18 @@ const update = (req, res) => {
 };
 
 const destroy = (req, res) => {
-    const id = req.params.id;
-    res.send(`Cancellazione del post ${id}`);
+     const id = parseInt(req.params.id);
+
+    const index = posts.findIndex(p => p.id === id);
+    if (index === -1) {
+        return res.status(404).json({ error: "Post not found" });
+    }
+
+    posts.splice(index, 1);
+
+    console.log(posts);
+
+    res.sendStatus(204);
 };
 
 module.exports = {
